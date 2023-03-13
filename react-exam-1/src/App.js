a97b598fd1845aa032cc1b50b39e54cc74c3bc15
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-13 19:32:43 +0900

    기본 구조 생성

---
+++ react-exam-1/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  let name = "Happy";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
