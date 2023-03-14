76864822a23d4e6be71c1182e91975dcbec700c3
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-14 16:58:24 +0900

    Props 사용해보기

--- react-exam-1/src/App.js
+++ react-exam-1/src/App.js
@@ -1,16 +1,28 @@
 // import "./App.css";
+import Container from "./Container";
 import Counter from "./Counter";
 import MyHeader from "./MyHeader";
 
 function App() {
-  const number = 5;
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+  };
 
   return (
-    <div>
+    <Container>
       <MyHeader />
-      <Counter />
-    </div>
+      <Counter {...counterProps} />
+    </Container>
   );
 }
+
+// initialValue가 props에 없을 시 기본값
+Counter.defaultProps = {
+  initialValue: 0,
+};
 
 export default App;
--- react-exam-1/src/Counter.js
+++ react-exam-1/src/Counter.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
+import OddEvenResult from "./OddEvenResult";
 
-const Counter = () => {
-  // 0에서 출발
-  // 1씩 증가
-  // 1씩 감소
+const Counter = ({ initialValue }) => {
   // count: 상태의 값
   // setCount: 상태를 변화(업데이트)
   // useState(): 초기값
 
-  console.log("counter 호출!");
+  // 리액트의 컴포넌트가 리랜더링이 되는 경우
+  // 1. 컴포넌트 본인이 관리하고 본인이 가진 State가 바뀔 때
+  // 2. 나에게 내려오는 props가 바뀔 때
+  // 3. 내 부모가 리렌더가 되는 경우
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialValue);
 
   const onIncrease = () => {
     setCount(count + 1);
@@ -25,6 +26,7 @@ const Counter = () => {
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
+      <OddEvenResult count={count} />
     </div>
   );
 };
