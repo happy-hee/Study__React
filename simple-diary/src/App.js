67d8ddc30eda1e5de57438a6d13ac1cfa9c9d9cd
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-15 20:52:25 +0900

    일기장 리스트 렌더링(조회)

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "happy1",
+    content: "내용1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "happy2",
+    content: "내용2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "happy3",
+    content: "내용3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
