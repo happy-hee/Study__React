eea07581b4fc61b9c7944b18521877b5ded86d93
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-04-05 12:56:19 +0900

    React에서 API 호출하기

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,14 +1,37 @@
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   //일기 데이터
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   // 새로운 일기 추가
   const onCreate = (author, content, emotion) => {
@@ -46,7 +69,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
