9ce36fb2809ce559598bbbadb41af138c61aa0d4
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-04-17 15:37:41 +0900

    useReduce와 React.createContext를 통한 최적화

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -9,58 +9,93 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
+import React, { useReducer, useRef } from "react";
 
-// compononts
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+// dispatch 함수들(onCreate, onRemove, onEdit)도 context를 생성해서 공급
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton text="왼쪽버튼" onClick={() => alert("왼쪽클릭")} />
-          }
-          rightChild={
-            <MyButton text="오른쪽버튼" onClick={() => alert("오른쪽클릭")} />
-          }></MyHeader>
-        <h2>App.js</h2>
+  const [data, dispatch] = useReducer(reducer, []);
 
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭!!");
-          }}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭!!");
-          }}
-          type={"negative"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭!!");
-          }}
-          type={"default"}
-        />
-        <Routes>
-          {/* Route : url 경로와 컴포넌트를 mapping 시켜주는 컴포넌트 */}
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          {/* path 경로 뒤에 콜론을 사용해서 뒤에있는 값을 전달하겠다고 선언 */}
-          <Route path="/diary:id" element={<Diary />} />
-        </Routes>
-        {/* React Route는 별도의 컴포넌트를 이용해서 페이지 이동 (예시 : <RouteTest />) */}
-        {/* <RouteTest /> */}
-      </div>
-    </BrowserRouter>
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              {/* Route : url 경로와 컴포넌트를 mapping 시켜주는 컴포넌트 */}
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              {/* path 경로 뒤에 콜론을 사용해서 뒤에있는 값을 전달하겠다고 선언 */}
+              <Route path="/diary:id" element={<Diary />} />
+            </Routes>
+            {/* React Route는 별도의 컴포넌트를 이용해서 페이지 이동 (예시 : <RouteTest />) */}
+            {/* <RouteTest /> */}
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
