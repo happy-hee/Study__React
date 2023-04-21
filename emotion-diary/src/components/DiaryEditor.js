25dd620a05c8ba38bb0f5b6bc7bda99bc8954abb
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-04-21 20:25:38 +0900

    일기 작성 페이지 작업

---
+++ emotion-diary/src/components/DiaryEditor.js
@@ -0,0 +1,136 @@
+import { useRef, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "./../App.js";
+
+import MyHeader from "./MyHeader";
+import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "보통",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "완전 나쁨",
+  },
+];
+
+// 날짜를 YYYY-MM-DD 형식으로 반환
+// (달력 날짜와 같은 형식으로 출력하기 위해)
+const getStringDate = (date) => {
+  // toISOString : ISO 형식의 문자열을 반환 - YYYY-MM-DDTH어쩌구 형식으로 반환
+  // slice 사용해서 YYYY-MM-DD로 나올 수 있도록 한다.
+  return date.toISOString().slice(0, 10);
+};
+
+const DiaryEditor = () => {
+  const contentRef = useRef();
+  const [content, setContent] = useState("");
+  const [emotion, setEmotion] = useState(3);
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+
+  // EmotionItem 에서 받아온 emotion을 현재 emotion으로 설정
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
+
+  const handleSubmit = () => {
+    // 내용을 1글자 이상 작성하지 않으면 focus 되도록
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+
+    onCreate(date, content, emotion);
+    // replace: true -> 작성 후 뒤로가기를 눌렀을 때 작성 페이지로 돌아가지 않도록
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <MyHeader
+        headText={"새 일기 쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+
+      {/* 작성일 선택 */}
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+
+        {/* 감정 점수 선택 */}
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                // 현재 감정 상태(선택한 감정)이 같을 경우 isSelected를 true로 설정
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
+          </div>
+        </section>
+
+        {/* 일기 내용 */}
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={handleSubmit}
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
